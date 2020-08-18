28a6cf4652a48a7a0406be9748b2f1100f940255
baroneluiz <luizhbarone@gmail.com>
2020-08-18 11:36:18 -0300

    Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,TextInput,TouchableOpacity  } from 'react-native';
 
 import styles from './styles';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login Page</Text>
+    <View style={styles.container}>
+      <Text
+      style={styles.logo}
+      >Login</Text>
+
+     <br/>
+
+      <TextInput 
+      style={styles.campos}
+      placeholder="Digite seu login"
+      />
+
+      <TextInput 
+      style={styles.campos}
+      secureTextEntry={true}
+      placeholder="Digite sua senha"
+      />
+    <TouchableOpacity
+      style={styles.botao}
+      onPress={() =>{} }
+      >
+      <Text
+      style={styles.botaotext}
+      >Login</Text>
+    </TouchableOpacity>
+
+    <TouchableOpacity
+    
+      onPress={() =>{} }
+      >
+      <Text
+      style={styles.criartext}
+      >Criar conta</Text>
+    </TouchableOpacity>
+
+
     </View>
   );
 };
--- src/pages/Login/styles.tsx
+++ src/pages/Login/styles.tsx
@@ -2,8 +2,46 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex:1
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor: "#655DE6"
+    
   },
+  campos:{
+    marginTop: 10,
+    padding: 10,
+    width: 300,
+    backgroundColor: "#fff",
+    fontSize: 16,
+    borderRadius: 3,
+    fontWeight: 'bold'
+  },
+  logo:{
+    width:300,
+    margin:30,
+    textAlign:"center",
+    fontWeight: 'bold',
+    fontSize: 26,
+    color: "#fff"
+  },
+  botao:{
+    width:300,
+    height:42,
+    backgroundColor: '#77DD77',
+    marginTop:10,
+    borderRadius:4,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  botaotext:{
+    fontSize:16,
+    fontWeight: 'bold',
+    
+  },
+  criartext:{
+    marginTop:10
+  }
 });
 
 export default styles;
